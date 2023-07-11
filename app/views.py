34bb1ad3416fa87e3d878353face5e4e6247aa06
sravanivallepu <sravanivallepu00294@gmail.com>
2023-07-11 14:25:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.forms import *
+from django.http import HttpResponse
+def sf(request):
+    SFO=StudentForm()
+    d={'SFO':SFO}
+    if request.method=='POST':
+        SFD=StudentForm(request.POST)
+        if SFD.is_valid():
+            return HttpResponse(str(SFD.cleaned_data))
+        else:
+            return HttpResponse('Invalid data')
+    return render(request,'sf.html',d)
