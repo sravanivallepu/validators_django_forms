82db3f03dc4c28548908c627725686fbd86637d0
sravanivallepu <sravanivallepu00294@gmail.com>
2023-07-13 22:35:07 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 
 def validate_for_a(Svalue):
     if Svalue[0].lower()=='a':
@@ -11,20 +12,21 @@ class StudentForm(forms.Form):
     sname=forms.CharField(max_length=100,validators=[validate_for_a,validate_for_len])
     sage=forms.IntegerField()
     email=forms.EmailField()
-    remail=forms.EmailField()
+    Remail=forms.EmailField()
     url=forms.URLField()
+    mobileno=forms.CharField(max_length=10,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
 
     botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
 
     def clean(self):
         e=self.cleaned_data['email']
-        re=self.cleaned_data['remail']
+        re=self.cleaned_data['Remail']
 
         if e != re:
-            raise forms.ValidationError('emails not matched')
+            raise forms.ValidationError('emails are not matched')
         
     def clean_botcatcher(self):
         bot=self.cleaned_data['botcatcher']
 
         if len(bot)>0:
-            raise forms.ValidationError('bot')
+            raise forms.ValidationError('data is not entered by human')
